c63711c21b1f9b54bda017e2a0605c8571d39a69
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2016-02-19 07:51:06 +0900

    (WIP) (Not working) first implementation
    Mintty exits immediately after launching. Why?

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,52 @@
 module Main where
 
-import Lib
+import qualified Data.List as L
+import qualified Control.Monad as M
+import qualified System.Environment as E
+import qualified System.FilePath as F
+import qualified System.Process as P
+
+import Debug.Trace
 
 main :: IO ()
-main = someFunc
+main = do
+  msys2Root <- getMsys2Root
+  let cygpathPath = getCygpathPath msys2Root
+  let minttyPath = getMinttyPath msys2Root
+  targetDirectory <- getTargetDirectory
+  cygwinTargetDirectory <- runCygpath cygpathPath targetDirectory
+  runMintty minttyPath cygwinTargetDirectory
+
+
+getMsys2Root :: IO String
+getMsys2Root =
+  E.getEnv "MSYS2_ROOT"
+
+
+getCygpathPath :: String -> String
+getCygpathPath msys2Root =
+  F.joinPath [msys2Root, "usr", "bin", "cygpath.exe"]
+
+
+getMinttyPath :: String -> String
+getMinttyPath msys2Root =
+  F.joinPath [msys2Root, "usr", "bin", "mintty.exe"]
+
+
+getTargetDirectory :: IO String
+getTargetDirectory =
+  head <$> E.getArgs
+
+
+runCygpath :: String -> String -> IO String
+runCygpath cygpathPath targetDirectory = do
+  result <- P.readProcess cygpathPath [targetDirectory] ""
+  if L.isSuffixOf "\n" result
+    then return $ init result
+    else return $ result
+
+
+runMintty :: String -> String -> IO ()
+runMintty minttyPath cygwinTargetDirectory = do
+  traceIO $ P.showCommandForUser minttyPath ["/bin/sh", "-lc", "'cd \"" ++ cygwinTargetDirectory ++ "\" ; exec bash'"]
+  M.void $ P.spawnProcess minttyPath ["/bin/sh", "-lc", "'cd \"" ++ cygwinTargetDirectory ++ "\" ; exec bash'"]
