38279e9d6d006b8935c839600c806868cca4f012
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2016-02-20 14:43:36 +0900

    Much simpler implementation
    Found what I have to do is just launch mintty at the given directory

--- app/Main.hs
+++ app/Main.hs
@@ -9,28 +9,8 @@ import qualified System.Process as P
 import Debug.Trace
 
 main :: IO ()
-main = do
-  msys2Root <- getMsys2Root
-  let cygpathPath = getCygpathPath msys2Root
-  let minttyPath = getMinttyPath msys2Root
-  targetDirectory <- getTargetDirectory
-  cygwinTargetDirectory <- runCygpath cygpathPath targetDirectory
-  runMintty minttyPath cygwinTargetDirectory
-
-
-getMsys2Root :: IO String
-getMsys2Root =
-  E.getEnv "MSYS2_ROOT"
-
-
-getCygpathPath :: String -> String
-getCygpathPath msys2Root =
-  F.joinPath [msys2Root, "usr", "bin", "cygpath.exe"]
-
-
-getMinttyPath :: String -> String
-getMinttyPath msys2Root =
-  F.joinPath [msys2Root, "usr", "bin", "mintty.exe"]
+main =
+  getTargetDirectory >>= runMinttyAt
 
 
 getTargetDirectory :: IO String
@@ -38,15 +18,7 @@ getTargetDirectory =
   head <$> E.getArgs
 
 
-runCygpath :: String -> String -> IO String
-runCygpath cygpathPath targetDirectory = do
-  result <- P.readProcess cygpathPath [targetDirectory] ""
-  if L.isSuffixOf "\n" result
-    then return $ init result
-    else return $ result
-
-
-runMintty :: String -> String -> IO ()
-runMintty minttyPath cygwinTargetDirectory = do
-  traceIO $ P.showCommandForUser minttyPath ["/bin/sh", "-lc", "'cd \"" ++ cygwinTargetDirectory ++ "\" ; exec bash'"]
-  M.void $ P.spawnProcess minttyPath ["/bin/sh", "-lc", "'cd \"" ++ cygwinTargetDirectory ++ "\" ; exec bash'"]
+runMinttyAt :: String -> IO ()
+runMinttyAt targetDirectory = do
+  let mintty = (P.proc "mintty" []){ P.cwd = Just targetDirectory }
+  M.void $ P.createProcess mintty
