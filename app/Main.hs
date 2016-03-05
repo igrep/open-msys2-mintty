29baa28e34b291a5276c92335866e818227bc8ac
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2016-03-05 19:11:14 +0900

    set title as current directory

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import qualified Control.Monad as M
 import qualified System.Environment as E
+import qualified System.FilePath as F
 import qualified System.Process as P
 
 main :: IO ()
@@ -16,5 +17,5 @@ getTargetDirectory =
 
 runMinttyAt :: String -> IO ()
 runMinttyAt targetDirectory = do
-  let mintty = (P.proc "mintty" []){ P.cwd = Just targetDirectory }
+  let mintty = (P.proc "mintty" ["--title", F.takeFileName targetDirectory]){ P.cwd = Just targetDirectory }
   M.void $ P.createProcess mintty
